2ad4e5b3d15c058991615a1d023133203ced6046
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-01-18 19:52:40 -0800

    Update assignment1.py

--- src/assignments/assignment1.py
+++ src/assignments/assignment1.py
@@ -4,11 +4,11 @@ def add_numbers(val1, val2):
 
 def multiply_numbers(val1, val2):
     '''Type the correct code to multiply two numbers after the return statement.'''
-    return
+    return val11 * val2
 
 def square_number(val1):
     '''
     Type the correct code to square val1 after the return statement.
     TO GET CREDIT YOU MUST USE THE PYTHON BUILT-IN FUNCTIONALITY FOR SQUARING NUMBERS!
     '''
-    return
+    return val1 ** 2
