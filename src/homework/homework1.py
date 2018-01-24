5e4954529ca150bf682efb12605fe0b4ee68a720
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-01-24 16:43:05 -0600

    Update homework1.py
    Code asks the user to input total number of seconds that has past since midnight.  After getting the input from user, the code first uses the //  operator (// 3600) to output only the whole number of 1 for the number of hours.  Then  to compute the number of minutes, the code again refers back to the total number of seconds that were input  and finds the remainder left when total seconds are divided by 3600 using the modulus operand (%) after which that number is divided by 60 again only getting the whole number (//).  Finally for the number of seconds, the total number of seconds can be found by 1st getting the remainder of seconds using % 3600 (200 seconds left), and eliminating the minutes so that only seconds remain using % 60 since there's 60 seconds in a minute.

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -4,6 +4,11 @@ def get_hours_since_midnight(seconds):
     For example, given 3800 seconds the total hours is 1
     '''
     return
+#get a number of seconds since midnight from the user
+get_hours_since_midnight = float(input('Enter total number of seconds since midnight: '))
+#determine the number of hours
+hours = get_hours_since_midnight // 3600
+print('Hours:', hours)
 
 '''
 IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
@@ -14,13 +19,17 @@ def get_minutes(seconds):
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 3
     '''
-
     return
+#determine the number of minutes
+mins = (get_hours_since_midnight % 3600) // 60
+print('Minutes:', mins)
 
 def get_seconds(seconds):
     '''
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 20
     '''
-
     return
+#determine the number of seconds
+secs = (get_hours_since_midnight % 3600) % 60
+print('Seconds:', secs)
