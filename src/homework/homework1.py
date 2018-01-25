4836930ad1e52c192ec15c09aa8a0bc1f793612d
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-01-24 21:51:44 -0600

    Update homework1.py
    Mr Gonzalez, I attempted to make the changes as you suggested.  I now understand that you want us to define a function, and then call the return function.  I don't follow how the code in this updated file will return the values of 1.0 hours, 3.0 minutes and 20 seconds since I don't see where the value of 3800 would be entered.  Nonetheless, here is my latest updated homework1.py file.  Again, I plan on stopping by your office before class tomorrow to get more help.

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -3,13 +3,8 @@ def get_hours_since_midnight(seconds):
     Type the code to calculate total hours given n(number) of seconds
     For example, given 3800 seconds the total hours is 1
     '''
-    return
-#get a number of seconds since midnight from the user
-get_hours_since_midnight = float(input('Enter total number of seconds since midnight: '))
-#determine the number of hours
-hours = get_hours_since_midnight // 3600
-print('Hours:', hours)
-
+    
+    return seconds // 3600
 '''
 IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
 '''
@@ -19,17 +14,12 @@ def get_minutes(seconds):
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 3
     '''
-    return
-#determine the number of minutes
-mins = (get_hours_since_midnight % 3600) // 60
-print('Minutes:', mins)
+    return (seconds % 3600) // 60
+
 
 def get_seconds(seconds):
     '''
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 20
     '''
-    return
-#determine the number of seconds
-secs = (get_hours_since_midnight % 3600) % 60
-print('Seconds:', secs)
+    return (seconds % 3600) % 60
